30d664c880341f437f6a47a3377e0659f72a1248
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-07-17 17:09:39 -0300

    criar dicionario e id,nome do jogador

--- parser.py
+++ parser.py
@@ -1,1 +1,15 @@
 f = open('Quake.txt', 'r')
+player = {}
+
+for line in f:
+    line = line.strip() #remover espaços em brancos
+    
+    if line.count("ClientUserinfoChanged"): #armazenar nome do jogador numa variavel
+    
+        line2 = line.split("\\")  #dividir a linha em indices pela \\
+        nomeJogador = line2[1]
+        line = line.split(" ") #dividir a linha em indices pelo espaõ
+        idJogador = line[2]
+        player = {}
+        player['id'] = idJogador
+        player['nome'] = nomeJogador
